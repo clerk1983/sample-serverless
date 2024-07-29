6750e02a9a981044d8cddbc8b2a791fb45442896
CLERK1983 <41065074+clerk1983@users.noreply.github.com>
2024-07-29 21:55:59 +0900

    feat RestAPI

--- src/costNotifier.js
+++ src/costNotifier.js
@@ -2,10 +2,11 @@ import { IncomingWebhook } from '@slack/webhook'
 import AWS from 'aws-sdk'
 import moment from 'moment'
 
-export async function handler(event, context) {
+export async function handler(evnet, context) {
   const now = moment()
   const start = now.format('YYYY-MM-01')
   const end = now.add(1, 'months').format('YYYY-MM-01')
+
   const ce = new AWS.CostExplorer({ region: 'us-east-1' })
   const params = {
     TimePeriod: {
@@ -15,11 +16,10 @@ export async function handler(event, context) {
     Granularity: 'MONTHLY',
     Metrics: ['UnblendedCost'],
   }
-  const constAndUsage = await ce.getCostAndUsage(params).promise()
-  const usdCost = constAndUsage.ResultsByTime[0].Total.UnblendedCost.Amount
+  const costAndUsage = await ce.getCostAndUsage(params).promise()
+  const usdCost = costAndUsage.ResultsByTime[0].Total.UnblendedCost.Amount
+
   const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL
   const slackWebhook = new IncomingWebhook(slackWebhookUrl)
-  await slackWebhook.send({
-    text: `今月のAWS利用料(${start}-${end})：USD ${usdCost} `,
-  })
+  await slackWebhook.send(`今月のAWS使用料：${usdCost}ドル`)
 }
--- src/taskHandler.js
+++ src/taskHandler.js
@@ -1,34 +1,45 @@
 import { DynamoDB } from 'aws-sdk'
 import crypto from 'crypto'
 
-export async function post(event, context) {
-  const requestBody = JSON.parse(event.body)
-  const item = {
-    id: { S: crypto.randomUUID() },
-    title: { S: requestBody.title },
-  }
-  const dynamoDB = new DynamoDB({ region: 'ap-northeast-1' })
-  await dynamoDB
-    .putItem({
-      TableName: 'tasks',
-      Item: item,
-    })
-    .promise()
-  return item
-}
+export async function list(evnet, context) {
+  const dynamodb = new DynamoDB({
+    region: 'ap-northeast-1',
+  })
 
-export async function list(event, context) {
-  const dynamoDB = new DynamoDB({ region: 'ap-northeast-1' })
-  const result = await dynamoDB
+  const result = await dynamodb
     .scan({
       TableName: 'tasks',
     })
     .promise()
+
   const tasks = result.Items.map(item => {
     return {
       id: item.id.S,
       title: item.title.S,
     }
   })
-  return { tasks }
+
+  return { tasks: tasks }
+}
+
+export async function post(event, context) {
+  const requestBody = JSON.parse(event.body)
+
+  const item = {
+    id: { S: crypto.randomUUID() },
+    title: { S: requestBody.title },
+  }
+
+  const dynamodb = new DynamoDB({
+    region: 'ap-northeast-1',
+  })
+
+  await dynamodb
+    .putItem({
+      TableName: 'tasks',
+      Item: item,
+    })
+    .promise()
+
+  return item
 }
